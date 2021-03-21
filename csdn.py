e5921f8bd683bcb8da6a67bbb354416438c80119
lqkxs3608 <67536525+lqkxs3608@users.noreply.github.com>
2021-03-21 09:13:02 +0800

    Update csdn.py

--- csdn.py
+++ csdn.py
@@ -14,18 +14,16 @@ def csdn_signin(cookie):
     res = json.loads(resp)  # 将json转化为数组形式
     message = res['message']  # 返回签到的结果
     issign = res['data']['isSigned']
-    if message == '成功' and issign:
-        t = res['data']['msg']
-        url = "https://me.csdn.net/api/LuckyDraw_v2/signInfo?product=&&type="
-        resp1 = requests.get(url, headers=headers).content.decode("unicode_escape")
-        resp1 = json.loads(resp1)  # 将json转化为数组形式
-        msg = t + '\n' + resp1['data']['msg']
-        signdays = resp1['data']['star']
-        if signdays == 5:
-            sign = requests.post("https://me.csdn.net/api/LuckyDraw_v2/goodLuck", headers=headers).content.decode(
+   
+    t = res['data']['msg']
+    url = "https://me.csdn.net/api/LuckyDraw_v2/signInfo?product=&&type="
+    resp1 = requests.get(url, headers=headers).content.decode("unicode_escape")
+    resp1 = json.loads(resp1)  # 将json转化为数组形式
+    msg = t + '\n' + resp1['data']['msg']
+    signdays = resp1['data']['star']
+    if signdays == 5:
+        sign = requests.post("https://me.csdn.net/api/LuckyDraw_v2/goodLuck", headers=headers).content.decode(
                 "unicode_escape")
-            option = json.loads(sign)['data']['msg']
-            msg = msg + '\n' + option
-    else:
-        msg = '签到失败，请检查cookies'
+        option = json.loads(sign)['data']['msg']
+        msg = msg + '\n' + option
     return msg
