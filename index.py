e7cdc1b9a52c48fd14afe5de16e8d98453605e63
lqkxs3608 <kafeidou@aliyun.com>
2021-03-24 21:31:18 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,10 +1,27 @@
 # -- coding: utf-8 --
+from queue import Queue
+from threading import Thread
 from pojie import pojie_signin
 from csdn import csdn_signin
 from tyyp import tyyp_signin
 from toollu import toollu_signin
 
-csdn_signin()
-tyyp_signin()
-toollu_signin()
-pojie_signin()
+q = Queue()
+q.put(csdn_signin())
+q.put(tyyp_signin())
+q.put(toollu_signin())
+q.put(pojie_signin())
+
+
+class SignIn(Thread):
+    def __init__(self, input):
+        self.input = input
+        Thread.__init__(self)
+
+    def run(self):
+        self.input.get()
+
+
+for i in range(3):
+    sign = SignIn(q)
+    sign.start()
