2b788dd60ec44ddc4e6d43bb4d5b60401da0dc42
lqkxs3608 <kafeidou@aliyun.com>
2021-03-24 21:36:09 +0800

    Update index.py

--- index.py
+++ index.py
@@ -6,23 +6,28 @@ from csdn import csdn_signin
 from tyyp import tyyp_signin
 from toollu import toollu_signin
 
-q = Queue()
-q.put(csdn_signin())
-q.put(tyyp_signin())
-q.put(toollu_signin())
-q.put(pojie_signin())
+tyyp_signin()
+csdn_signin()
+toollu_signin()
+pojie_signin()
 
-
-class SignIn(Thread):
-    def __init__(self, input):
-        self.input = input
-        Thread.__init__(self)
-
-    def run(self):
-        self.input.get()
-
-
-for i in range(3):
-    sign = SignIn(q)
-    sign.start()
-    sign.join()
+# q = Queue()
+# q.put(csdn_signin())
+# q.put(tyyp_signin())
+# q.put(toollu_signin())
+# q.put(pojie_signin())
+#
+#
+# class SignIn(Thread):
+#     def __init__(self, input):
+#         self.input = input
+#         Thread.__init__(self)
+#
+#     def run(self):
+#         self.input.get()
+#
+#
+# for i in range(3):
+#     sign = SignIn(q)
+#     sign.start()
+#     sign.join()
